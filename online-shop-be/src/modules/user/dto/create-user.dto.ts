d281c390f82e74c4b99d2c196cdfe97d0541a771
Anh <ainchan29@gmail.com>
2025-03-27 09:35:43 +0700

    Finishing DTO for create and get user

---
+++ online-shop-be/src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,48 @@
+import {
+    IsEmail,
+    IsString,
+    MinLength,
+    MaxLength,
+    Matches,
+    IsNotEmpty,
+  } from 'class-validator';
+
+import { Match } from './custom-class-validator';
+
+export class GetUserDTO {
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(20)
+    readonly fullName: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    readonly email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character'})
+    readonly password: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    @Match('password', {message: 'Confirm password must match password'})
+    readonly confirmPassword: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(10)
+    @MaxLength(11)
+    @Matches(/^\d+$/, {message: 'Phone number must be number only'})
+    readonly phone: string;
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(20)
+    readonly address: string;
+}
--- online-shop-be/src/modules/user/dto/get-user.dto.ts
+++ online-shop-be/src/modules/user/dto/get-user.dto.ts
@@ -3,16 +3,19 @@ import {
     IsString,
     MinLength,
     MaxLength,
-    Matches
+    Matches,
+    IsNotEmpty,
   } from 'class-validator';
 
 export class GetUserDTO {
     @IsEmail()
+    @IsNotEmpty()
     readonly email: string;
-    
+
+    @IsNotEmpty()
     @IsString()
     @MinLength(4)
     @MaxLength(20)
-    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'Invalid password'})
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character'})
     readonly password: string;
 }
