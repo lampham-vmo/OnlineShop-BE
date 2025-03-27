c5538658c158010ca59dd6f9f004dc39d65c3744
Anh <anhthh3@vmogroup.com>
2025-03-27 15:50:37 +0700

    Login sign up implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/user/entities/user.entity';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'postgre',
+      password: 'admin',
+      database: 'online_shop_db',
+      entities: [User],
+      synchronize: true,
+    }),UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
