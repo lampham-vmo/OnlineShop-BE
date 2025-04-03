4eb0947a502dab54e4955c37552e2b435b9c0668
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2025-04-03 14:06:54 +0700

    docs: add Swagger to main.ts file and update package.json

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './common/exceptions/global-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('VMO Online Shop API')
+  .setDescription('These are APIs with Nestjs')
+  .setVersion('1.0.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api', app, document)
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new GlobalExceptionFilter(app.get(HttpAdapterHost)));
   await app.listen(process.env.PORT ?? 3000);
