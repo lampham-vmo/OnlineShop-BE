5922ec91bc2bff82e5d9c243a07d76a39ee599a1
ITL2k3 <91226590+ITL2k3@users.noreply.github.com>
2025-04-08 11:15:40 +0700

    #enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { GlobalExceptionFilter } from './common/exceptions/global-exception.filt
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Cho phép gửi cookie
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new GlobalExceptionFilter(app.get(HttpAdapterHost)));
   await app.listen(process.env.PORT ?? 3000);
