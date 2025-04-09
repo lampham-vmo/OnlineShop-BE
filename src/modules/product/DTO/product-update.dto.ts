dbb8707f8141427a82b50af977254f3b5e2add15
Nguyen Phu Quan <quannp3@vmogroup.com>
2025-04-09 16:38:20 +0700

    resolve conflict

--- src/modules/product/DTO/product-update.dto.ts
+++ src/modules/product/DTO/product-update.dto.ts
@@ -1,6 +1,6 @@
-import { OmitType, PartialType } from "@nestjs/mapped-types";
-import { ProductRequest } from "./requests/product.request";
+import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { ProductRequest } from './requests/product.request';
 
 export class ProductUpdateDto extends PartialType(
-    OmitType(ProductRequest, ['categoryId']),
+  OmitType(ProductRequest, ['categoryId']),
 ) {}
