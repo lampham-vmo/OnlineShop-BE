04e1bb2299d1b7afe3b00b44820bc93d44461354
ITL2k3 <91226590+ITL2k3@users.noreply.github.com>
2025-04-10 15:40:02 +0700

    #config dto role

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Get, Patch, Post, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpStatus, Patch, Post, UseGuards } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { AuthGuard } from 'src/common/guard/auth.guard';
 import { RoleGuard } from 'src/common/guard/role.guard';
@@ -6,6 +6,7 @@ import { Role } from './entities/role.entity';
 import { CreateRoleDTO } from './dto/create-role.dto';
 import { APIResponseDTO } from 'src/common/dto/response-dto';
 import { UpdateRoleDTO } from './dto/update-role-dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Controller('role')
 export class RoleController {
@@ -13,22 +14,26 @@ export class RoleController {
     constructor(private readonly roleService: RoleService) {}
       @Get()
       @UseGuards(AuthGuard, RoleGuard)
-      async findAll(): Promise<Role[]> {
-        return  await this.roleService.getAllRole();
-       
+      @ApiProperty()
+      async findAll(): Promise<APIResponseDTO<Role[]>> {
+        const roles =   await this.roleService.getAllRole();
+        return new APIResponseDTO<Role[]>(true, HttpStatus.OK, roles)
       }
 
       @Post()
       @UseGuards(AuthGuard, RoleGuard)
-      async addRole(@Body() createRoleDTO: CreateRoleDTO) : Promise<APIResponseDTO<string> | BadRequestException>{
-        console.log(createRoleDTO);
-        return await this.roleService.createARole(createRoleDTO)
+      @ApiProperty()
+      async addRole(@Body() createRoleDTO: CreateRoleDTO) : Promise<APIResponseDTO<string>>{
+        const result = await this.roleService.createARole(createRoleDTO)
+        return new APIResponseDTO<string>(true, HttpStatus.CREATED, result)
       }
 
       @Patch()
       @UseGuards(AuthGuard, RoleGuard)
+      @ApiProperty()
       async updateRole(@Body() updateRoleDTO: UpdateRoleDTO){
-        return await this.roleService.updateARole(updateRoleDTO)
+        const result = await this.roleService.updateARole(updateRoleDTO)
+        return new APIResponseDTO<string>(true, HttpStatus.CREATED, result)
       }
 
      
--- src/modules/role/role.service.ts
+++ src/modules/role/role.service.ts
@@ -38,7 +38,7 @@ export class RoleService implements OnModuleInit {
 
   async createARole(
     newRole: CreateRoleDTO,
-  ): Promise<APIResponseDTO<string> | BadRequestException> {
+  ): Promise<string> {
     const { name, description, permissionIds } = newRole;
     const role = new Role();
     role.name = name;
@@ -53,13 +53,14 @@ export class RoleService implements OnModuleInit {
         });
       }
       this.roleRepository.save(role);
-      return new APIResponseDTO(true, 200, 'Sucessfully create a role');
+      return "Sucessfully create a role"
+      
     }
   }
 
   async updateARole(
     updatedRole: UpdateRoleDTO,
-  ): Promise<APIResponseDTO<string> | BadRequestException> {
+  ): Promise<string> {
     const { id } = updatedRole
     const role = await this.roleRepository.findOne({
       where: { id },
@@ -91,7 +92,7 @@ export class RoleService implements OnModuleInit {
     await this.roleRepository.save(role);
 
 
-    return new APIResponseDTO<string>(true, 200, 'Successfully updated a user');
+    return  'Successfully updated a user'
   }
 
   async getAllRole(): Promise<Role[]> {
